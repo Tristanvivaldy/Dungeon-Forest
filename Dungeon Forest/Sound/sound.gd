08f092a3d81cbed9635b651bbe59e854b4ef9da8
Tristan Vivaldy <90676418+Tristanvivaldy@users.noreply.github.com>
2023-12-06 18:52:31 +0700

    Add files via upload

---
+++ Dungeon Forest/Sound/sound.gd
@@ -0,0 +1,14 @@
+extends Node
+
+onready var click = $click
+onready var hover = $hover
+onready var enemy_hit = $enemy_hit
+onready var game_over = $game_over
+onready var hit_sword = $hit_sword
+onready var kena_hit = $kena_hit
+onready var next_dialogue = $next_dialogue
+onready var open_chest = $open_chest
+onready var open_door = $open_door
+onready var potion = $potion
+onready var pause_sound = $pause_sound
+onready var dash = $dash
